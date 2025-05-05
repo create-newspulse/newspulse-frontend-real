ea48ea1d65a279de6bf07aad60315924517b7c60
create-newspulse <newspulse.ads@gmail.com>
2025-05-05 23:56:14 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
-import * as gtag from '../lib/gtag'; // ✅ Keep this once
+import * as gtag from '../lib/gtag';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
@@ -19,7 +19,6 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      {/* ✅ Google Analytics Script */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-9YPR93VVX4`}
@@ -28,15 +27,14 @@ export default function MyApp({ Component, pageProps }) {
         id="gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
+          __html: \`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-9YPR93VVX4');
-          `,
+          \`,
         }}
       />
-
       <Component {...pageProps} />
     </>
   );
