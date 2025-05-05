085b0237304716521bcc258d176c3e6b13f8159a
create-newspulse <newspulse.ads@gmail.com>
2025-05-05 12:26:41 +0530

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -2,8 +2,7 @@ export default function NewsPage() {
   return (
     <div>
       <h1>News Page</h1>
-      {/* Add your content or import components */}
+      <p>This is the news list page.</p>
     </div>
   );
 }
-
