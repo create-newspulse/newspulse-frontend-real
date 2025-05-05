3e6bbc2020c1a8e002e46177876c1cc8bb317a04
create-newspulse <newspulse.ads@gmail.com>
2025-05-05 20:13:09 +0530

    Update gtag.js

--- pages/lib/gtag.js
+++ pages/lib/gtag.js
@@ -1,8 +1,6 @@
 // lib/gtag.js
-
 export const GA_TRACKING_ID = 'G-9YPR93VVX4';
 
-// Log page views
 export const pageview = (url) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
