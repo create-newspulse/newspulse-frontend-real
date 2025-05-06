7e71979c326f6a0cff1273d6ad66d33d5fdbe9cc
create-newspulse <newspulse.ads@gmail.com>
2025-05-07 01:33:35 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       animation: {
-        marquee: 'marquee 20s linear infinite',
+        'fade-in': 'fadeIn 0.8s ease-out',
       },
       keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-      },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+      }
     },
   },
-};
+  plugins: [],
+}
