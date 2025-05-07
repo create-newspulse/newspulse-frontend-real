5eb295fcf09dfdb2275d71b75e736091df2fc319
create-newspulse <newspulse.ads@gmail.com>
2025-05-07 09:12:00 +0530

    Create next.config.js

---
+++ components/next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+module.exports = {
+  images: {
+    domains: [
+      'static.toiimg.com',
+      'cdn.cnn.com',
+      'ichef.bbci.co.uk',
+      'media.guim.co.uk',
+      'images.unsplash.com',
+      'i.ndtvimg.com',
+      'www.livemint.com',
+      's.yimg.com',
+      'img.etimg.com'
+    ],
+  },
+};
