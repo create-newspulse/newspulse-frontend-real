ce8dc6822846ce1ebfa357d376b2aeb326164448
create-newspulse <newspulse.ads@gmail.com>
2025-05-07 10:53:48 +0530

    Update next.config.js

--- components/next.config.js
+++ components/next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 module.exports = {
-  images: {
-    domains: ['cdn.cnn.com', 'static.toiimg.com', 'images.unsplash.com', 'newsapi.org', 'media.newspulse.co.in'],
-  },
-};
+    images: {
+      domains: ['cdn.cnn.com', 'static.toiimg.com', 'images.unsplash.com', 'newsapi.org', 'media.newspulse.co.in'],
+    },
+  };
+  
