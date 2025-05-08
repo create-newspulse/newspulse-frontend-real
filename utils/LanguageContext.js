4dd073f8728b67e84f9ba543379a2263c1cc9ef6
create-newspulse <newspulse.ads@gmail.com>
2025-05-09 00:21:06 +0530

    Create LanguageContext.js

---
+++ utils/LanguageContext.js
@@ -0,0 +1,16 @@
+// /utils/LanguageContext.js
+import { createContext, useContext, useState } from 'react';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('english'); // default
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
