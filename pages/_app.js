bae0906edc14b966da412ec38a71ea7924d42785
create-newspulse <newspulse.ads@gmail.com>
2025-05-08 23:17:31 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
 // pages/_app.js
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Hindi Font: Noto Sans Devanagari */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Devanagari&display=swap"
+          rel="stylesheet"
+        />
+        {/* Gujarati Font: Shruti - System fallback */}
+        <style>{`
+          @font-face {
+            font-family: 'Shruti';
+            src: local('Shruti');
+          }
+        `}</style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
