16202d771eb66bae79cffe6346ae7539c6ad637b
create-newspulse <newspulse.ads@gmail.com>
2025-05-10 00:29:09 +0530

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Google AdSense Script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9257940786279478"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
