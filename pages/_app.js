911d288fd2b8d9cdac88f5cb5e53211147a6d0ef
create-newspulse <newspulse.ads@gmail.com>
2025-05-09 22:49:58 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-// pages/_app.js
 import '../styles/globals.css';
-import { LanguageProvider } from '../utils/LanguageContext'; // ✅ import provider
+import { LanguageProvider } from '../utils/LanguageContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <LanguageProvider> {/* ✅ wrap the entire app */}
+    <LanguageProvider>
       <Component {...pageProps} />
     </LanguageProvider>
   );
