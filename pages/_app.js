95f6c0287bc256bf36cf2087af83d0fb86ef4b6b
create-newspulse <newspulse.ads@gmail.com>
2025-05-09 11:39:02 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
+// pages/_app.js
 import '../styles/globals.css';
-import { useLanguage } from '../utils/LanguageContext'; // ✅
+import { LanguageProvider } from '../utils/LanguageContext';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Devanagari&display=swap"
+          rel="stylesheet"
+        />
+        <style>
+          {`
+            @font-face {
+              font-family: 'Shruti';
+              src: local('Shruti');
+            }
+          `}
+        </style>
+      </Head>
       <Component {...pageProps} />
     </LanguageProvider>
   );
