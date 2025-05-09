3dc78afbb04970899aad984737123bbce6d4fefa
create-newspulse <newspulse.ads@gmail.com>
2025-05-09 11:53:47 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -13,4 +9,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
