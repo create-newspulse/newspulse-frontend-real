17fc913befb02fae86c0bf7c1a39aa8083fc2767
create-newspulse <newspulse.ads@gmail.com>
2025-05-09 17:24:41 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
-module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        hindi: ['"Noto Sans Devanagari"', 'sans-serif'],
-        gujarati: ['Shruti', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-};
+extend: {
+  fontFamily: {
+    gujarati: ['Shruti', 'sans-serif'],
+    hindi: ['"Noto Sans Devanagari"', 'sans-serif'],
+  }
+}
