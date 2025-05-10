450d5a94f39b3e237bcb82445cba41e27ec5f5b7
create-newspulse <newspulse.ads@gmail.com>
2025-05-10 21:12:22 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,25 @@ export default function Document() {
       <Head>
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-        
-        {/* Google AdSense Script */}
+
+        {/* Google AdSense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9257940786279478"
           crossOrigin="anonymous"
+        ></script>
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXXXXXX');
+            `,
+          }}
         ></script>
       </Head>
       <body>
