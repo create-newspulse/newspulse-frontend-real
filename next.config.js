badc4e2dca59c394ec1a08e5d720c31b628616a2
create-newspulse <newspulse.ads@gmail.com>
2025-05-10 19:22:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,44 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+
+module.exports = {
   reactStrictMode: true,
-}
-module.exports = nextConfig
+
+  images: {
+    domains: [
+      'cdn.newsapi.org',
+      'images.unsplash.com',
+      'media.licdn.com',
+      'static.toiimg.com',
+      'gnews.io',
+      'cdn.gulte.com',
+    ],
+  },
+
+  env: {
+    NEWSAPI_KEY: process.env.NEWSAPI_KEY,
+    NEWSDATA_API_KEY: process.env.NEWSDATA_API_KEY,
+    THENEWSAPI_TOKEN: process.env.THENEWSAPI_TOKEN,
+    MEDIASTACK_API_KEY: process.env.MEDIASTACK_API_KEY,
+    GNEWS_API_KEY: process.env.GNEWS_API_KEY,
+  },
+
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "X-Frame-Options",
+          value: "DENY",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "strict-origin-when-cross-origin",
+        },
+      ],
+    },
+  ],
+};
