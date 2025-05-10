7b80efb0497153307d7b2d95bf387edb58a9fb19
create-newspulse <newspulse.ads@gmail.com>
2025-05-10 17:25:23 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import '../styles/globals.css';
+import '../styles/BreakingTicker.css'; // ✅ Add this here
 import { LanguageProvider } from '../utils/LanguageContext';
 
 function MyApp({ Component, pageProps }) {
